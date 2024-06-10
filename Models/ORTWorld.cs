fc4dc6962dc6d3254231194d47c99a770b58a2da
TOMAS DAVID KLAWIR <48385206@ort>
2024-06-10 10:12:32 -0300

    View SelectPaquete hecha

--- Models/ORTWorld.cs
+++ Models/ORTWorld.cs
@@ -3,7 +3,7 @@ public static class ORTWorld{
     public static List <string> ListaHoteles {get; private set;} = new List<string> {"eurostars-madrid-tower", "le narcisse", "maldivas", "jamaica-hotel", "alpes suizos", "eilat royal beach", "hilton copacabana", "the palace", "nueva york", "intercontinental"};
     public static List <string> ListaAereos {get; private set;} = new List<string> {"latam", "american airlines", "turkish airlines", "sigapour airlines", "gol", "flybondi", "aerolineas argentinas", "Delta", "emirates airlines", "boa"};
     public static List <string> ListaExcursiones {get; private set;} = new List <string> {"bernabeu-paseo", "louvre", "playa maldivas", "Bamboo-bote", "ski", "paseo", "cristo redentor", "safari", "bicis", "bosforo"};
-    public static Dictionary<string, Paquete> Paquetes {get; private set;}
+    public static Dictionary<string, Paquete> Paquetes {get; private set;} = new Dictionary<string, Paquete>();
     public static bool IngresarPaquete(string destinoSeleccionado, Paquete paquete)
     {
         if(!Paquetes.Keys.Contains(destinoSeleccionado)){
--- obj/Debug/net8.0/TP04.AssemblyInfo.cs
+++ obj/Debug/net8.0/TP04.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TP04")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e77284bb00543a6a8c70612f0e0728deae06bcab")]
 [assembly: System.Reflection.AssemblyProductAttribute("TP04")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TP04")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
